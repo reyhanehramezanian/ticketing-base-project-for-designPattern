28488a22bdf7f44957bcf99a9cbf0f02492bb6bd
Reyhaneh Ramazanian Moghaddam <reyhaneh.ram83@gmail.com>
2026-02-11 16:08:59 +0330

    Update TicketService.java

--- src/edu/ticket/TicketService.java
+++ src/edu/ticket/TicketService.java
@@ -1,53 +1,20 @@
 package edu.ticket;
 
 public class TicketService {
-
-    private String state = "NEW";
-
+    
     public void handle(String channel, String type) {
+        Ticket ticket = TicketFactory.createTicket(channel, type);
+        
+        ticket.process();
+    }
 
-        if (state.equals("NEW")) {
-            System.out.println("Ticket created");
-
-            if (channel.equals("WEB")) {
-                System.out.println("Received from web");
-            } else if (channel.equals("EMAIL")) {
-                System.out.println("Received from email");
-            }
-
-            state = "ASSIGNED";
-        }
-
-        if (state.equals("ASSIGNED")) {
-            if (type.equals("BUG")) {
-                System.out.println("Assigned to engineering");
-            } else {
-                System.out.println("Assigned to support");
-            }
-            state = "IN_PROGRESS";
-        }
-
-        if (state.equals("IN_PROGRESS")) {
-            System.out.println("Working on ticket");
-
-            if (type.equals("BUG")) {
-                System.out.println("Sending bug response");
-            } else {
-                System.out.println("Sending generic response");
-            }
-
-            state = "RESOLVED";
-        }
-
-        if (state.equals("RESOLVED")) {
-            System.out.println("Ticket resolved");
-            state = "CLOSED";
-        }
-
-        if (state.equals("CLOSED")) {
-            System.out.println("Ticket closed");
-        }
-
-        System.out.println("Logging ticket handling");
+     public static void main(String[] args) {
+        TicketService service = new TicketService();
+        
+        System.out.println("--- Test 1: Web Bug ---");
+        service.handle("WEB", "BUG");
+        
+        System.out.println("\n--- Test 2: Email Support ---");
+        service.handle("EMAIL", "SUPPORT");
     }
 }
