ee976534756966a9463aefd8a8edc7ec460cdfbb
Reyhaneh Ramazanian Moghaddam <reyhaneh.ram83@gmail.com>
2026-02-11 16:17:33 +0330

    Update TicketService.java

--- src/edu/ticket/TicketService.java
+++ src/edu/ticket/TicketService.java
@@ -7,14 +7,4 @@ public class TicketService {
         
         ticket.process();
     }
-
-     public static void main(String[] args) {
-        TicketService service = new TicketService();
-        
-        System.out.println("--- Test 1: Web Bug ---");
-        service.handle("WEB", "BUG");
-        
-        System.out.println("\n--- Test 2: Email Support ---");
-        service.handle("EMAIL", "SUPPORT");
-    }
 }
